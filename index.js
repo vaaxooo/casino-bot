85d2bd0bcd4f47f86ea37f4e7d5bcc0bf1ea3ffc
vaaxooo <donjkee47@gmail.com>
2023-10-04 15:31:39 +0200

    - added ref

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ if (token === undefined) {
 
 const gameShortName = 'blowbetofficial'
 
+let telegramRefs = {}
+
 const bot = new Telegraf(token)
-bot.start((ctx) => ctx.replyWithGame(gameShortName))
+bot.start((ctx) => {
+	let user = ctx;
+	telegramRefs[user.from.id] = ctx.args[0];
+	ctx.replyWithGame(gameShortName)
+})
+
 bot.gameQuery((ctx) => {
 	let user = ctx;
+	let ref = telegramRefs[user.from.id]
 	let currentTimestamp = new Date().getTime();
 	let userData = {
 		telegram_id: user.from.id,
@@ -24,11 +32,12 @@ bot.gameQuery((ctx) => {
 		first_name: user.from.first_name || '',
 		username: user.from.username || '',
 		photo_url: user.from.photo_url || '',
-		last_name: user.from.last_name || ''
+		last_name: user.from.last_name || '',
+		ref: ref || ''
 	}
 
 	let hash = md5(JSON.stringify(userData) + process.env.SECRET);
-	let gameUrl = process.env.URL + "/auth?telegram_hash=" + hash + "&telegram_id=" + user.from.id + "&timestamp=" + currentTimestamp + "&lang=ru" + "&first_name=" + userInfo.first_name + "&username=" + userInfo.username + "&photo_url=" + userInfo.photo_url + "&last_name=" + userInfo.last_name
+	let gameUrl = process.env.URL + "/auth?telegram_hash=" + hash + "&telegram_id=" + user.from.id + "&timestamp=" + currentTimestamp + "&lang=ru" + "&first_name=" + userInfo.first_name + "&username=" + userInfo.username + "&photo_url=" + userInfo.photo_url + "&last_name=" + userInfo.last_name + "&ref=" + userInfo.ref;
 	ctx.answerGameQuery(gameUrl)
 })
 bot.launch()
